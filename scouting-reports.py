7460e5259056b304492867b56c26c1c432c6d82b
therealsmithy <liamsmith3434@gmail.com>
2025-02-06 12:30:41 -0500

    Set plan for scouting reports

---
+++ scouting-reports.py
@@ -0,0 +1,7 @@
+# Process
+#1. Scrape strengths and weaknesses for PFF
+#2. Scrape old ones if possible
+#3. TFIDVectorizer onto the data
+#4. Train XGBoost model
+#5. SHAP Models
+#6. https://dalex.drwhy.ai/python/
